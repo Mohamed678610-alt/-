9103923494896ea53cb201db7e1fc1bb5a6c0820
Mohamed678610-alt <74241864+Mohamed678610-alt@users.noreply.github.com>
2020-11-10 15:49:11 +0300

    Add files via upload

---
+++ الواجب الاول.cpp
@@ -0,0 +1,48 @@
+/*My name:mohamed hassanin mahjob
+Class:second
+Department:computer engineering
+*/
+#include <iostream>
+using namespace std;
+ main()
+{
+    char name;
+    cout<<"student's name:";cin>>name;
+    int ID;
+    cout<<"University ID:";cin>>ID;
+    switch(A,T);
+    case 1:
+    cout<<A;cin>>"It belongs to control engineering\n";
+    break;
+    case 2:
+    cout<<T;cin>>"It belongs to communications engineering\n";
+    break;
+    int grade;
+    cout << "Enter your grade: ";
+    cin >> grade;
+    else
+    if (grade >= 100 && grade <= 90)
+    cout << "You got Grade A\n";
+    else
+    if (grade< 90 && grade>80)
+    cout << "You got Grade B\n";
+    
+    else
+    if (grade<80 && grade>70)
+    cout << "You got Grade C\n";
+    
+    else
+    if (grade<70 && grade>60)
+    cout << "You got Grade D\n";
+    
+    else
+    if (grade<60 && grade>50)
+    cout << "You got Grade F\n";
+ 
+    system("pause");
+    return 0;
+}
+
+
+
+
